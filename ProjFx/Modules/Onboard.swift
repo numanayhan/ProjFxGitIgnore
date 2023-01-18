3537d7748506aa4e3cd5b125639a960c8774eecc
nayhan <>
2023-01-18 13:07:27 +0300

    git ignore test

---
+++ ProjFx/Modules/Onboard.swift
@@ -0,0 +1,17 @@
+//
+//  Onboard.swift
+//  ProjFx
+//
+//  Created by nayhan on 4.01.2023.
+//
+
+import UIKit
+
+class Onboard: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    } 
+
+}
